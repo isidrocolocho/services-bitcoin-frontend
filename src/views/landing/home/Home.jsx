5b8a5dfcedafedd1f16ae7881062b25d93ab1ed2
dannurias <danurias2003@gmail.com>
2025-01-28 14:29:31 -0600

    Modificacion de index, landing e imagenes.

--- src/views/landing/home/Home.jsx
+++ src/views/landing/home/Home.jsx
@@ -1,72 +1,57 @@
 import React from "react";
-import Testimonios from "./Testimonios"; // Importa el componente de testimonios si está en el mismo directorio
 
-export default function Home() {
+const Home = () => {
   return (
-    <div>
-      {/* Sección principal */}
-      <section className="h-screen max-w-7xl mx-auto px-4 py-12 flex flex-col justify-center">
-        <div className="grid grid-rows-2 md:grid-cols-2 md:grid-rows-1 gap-8 items-center h-full">
-          {/* Texto en la primera fila */}
-          <div className="md:pl-20 flex flex-col justify-center">
-            <h1 className="text-4xl md:text-5xl font-bold mb-4">
-              Tu salud, <br />
-              <span className="text-verde-aqua font-black italic">nuestra prioridad.</span>
-            </h1>
-            <p className="text-gray-600 mb-8">
-              Descubre una forma fácil y rápida de encontrar especialistas, agendar citas y gestionar tu cuidado médico desde cualquier dispositivo.
-            </p>
-            <div className="space-x-4">
-              <button className="bg-white text-black border-2 border-black hover:bg-gray-100 py-2 px-4 rounded">
-                Agendar Cita
-              </button>
-              <button className="bg-azul-navy text-white hover:bg-blue py-2 px-4 rounded">
-                Equipo Médico
-              </button>
-            </div>
-          </div>
-
-          {/* Imagen en la segunda fila */}
-          <div className="relative h-64 md:h-full flex items-center justify-center">
-            <img
-              src="/images/estetoscopio.jpg"
-              alt="Medical stethoscope heart"
-              className="w-full h-full object-contain"
-            />
-          </div>
-        </div>
-      </section>
-
-      {/* Sección de beneficios */}
-      <section className="flex flex-col md:flex-row items-center md:items-start justify-between px-8 py-16 bg-white">
-        <div className="w-full md:w-1/2 flex flex-col justify-center md:pl-20 pt-6 items-center md:items-start h-full">
-          <h2 className="text-3xl md:text-4xl font-bold italic text-verde-aqua text-center md:text-left">
-            Beneficios <span className="text-gray-900 not-italic">que</span> <br />
-            <span className="text-gray-900 not-italic">hacen la diferencia.</span>
-          </h2>
-          <p className="mt-4 text-gray-600 leading-relaxed text-center md:text-left">
-            Descubre cómo nuestro sistema simplifica tu<br />
-            experiencia médica con características clave y<br />
-            ventajas exclusivas.
+    <section className="relative h-screen flex flex-col items-center justify-center text-center text-white">
+      <div className="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
+        <video
+          className="min-w-full min-h-full absolute object-cover"
+          src="/images/vidBg.mp4"
+          type="video/mp4"
+          autoPlay
+          muted
+          loop
+        ></video>
+      </div>
+      <div className="video-content space-y-2 z-10">
+        <div class="text-center lg:text-left lg:w-1/2">
+          <h1 class="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
+            Main title of your landing page
+          </h1>
+          <p class="text-xl lg:text-2xl mt-6 font-light">
+            Free landing page template to promote your business startup and
+            generate leads for the offered services
           </p>
-          <a href="#" className="inline-block mt-6 text-teal-600 hover:underline font-medium">
-            Más Información →
-          </a>
+          <p class="mt-8 md:mt-12">
+            <button
+              type="button"
+              class="py-4 px-12 bg-[#0F3715] hover:bg-[#5E8F34] rounded text-white">
+              Get Started
+            </button>
+          </p>
+          <p class="mt-4 text-gray-600">Sed fermentum felis ut cursu</p>
         </div>
+      </div>
+      <style jsx>{`
+        .video-docker video {
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+        }
 
-        <div className="w-full md:w-1/2 mt-6 md:mt-0 flex justify-center items-center">
-          <div className="w-full h-64 md:h-96 overflow-hidden">
-            <img
-              src="/images/DiseñoBeneficios.png"
-              alt="Collage Doctores"
-              className="w-full h-full object-cover"
-            />
-          </div>
-        </div>
-      </section>
-
-      {/* Componente de testimonios */}
-      <Testimonios />
-    </div>
+        .video-docker::after {
+          content: "";
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          top: 0;
+          left: 0;
+          background: rgba(0, 0, 0, 0.6);
+          z-index: 1;
+        }
+      `}</style>
+    </section>
   );
-}
+};
+
+export default Home;
--- src/views/partials/Navbar.jsx
+++ src/views/partials/Navbar.jsx
@@ -6,15 +6,15 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="bg-celeste-aqua">
+    <header className="bg-[#0F3715]">
       <div className="max-w-7xl mx-auto flex justify-between items-center py-4 px-6">
         {/* Logo */}
         <div>
           <Link to="/">
             <img 
-              src="/images/Logo.png" 
+              src="/images/SatoshiLog.png" 
               alt="Logo" 
-              className="h-10 w-auto"
+              className="h-16 w-auto"
             />
           </Link>
         </div>
@@ -23,19 +23,19 @@ const Navbar = () => {
         <nav className="hidden md:flex space-x-8">
           <Link 
             to="/" 
-            className="text-azul-navy font-bold hover:text-cyan-600"
+            className="text-[#f2f2f2] font-bold hover:text-[#CCBB8E]"
           >
             Inicio
           </Link>
           <Link 
             to="/somos" 
-            className="text-azul-navy font-bold hover:text-cyan-600"
+            className="text-[#f2f2f2] font-bold hover:text-[#CCBB8E]"
           >
             ¿Quiénes somos?
           </Link>
           <Link 
             to="/servicios" 
-            className="text-azul-navy font-bold hover:text-cyan-600"
+            className="text-[#f2f2f2] font-bold hover:text-[#CCBB8E]"
           >
             Servicios
           </Link>
@@ -45,13 +45,13 @@ const Navbar = () => {
         <div className="hidden md:flex space-x-4">
           <Link 
             to="/registro" 
-            className="border border-gray-500 bg-white px-4 py-2 rounded-md hover:bg-gray-100"
+            className="border border-gray-500 bg-[#f2f2f2] px-4 py-2 rounded-md hover:bg-gray-400"
           >
             Registrarse
           </Link>
           <Link 
             to="/login" 
-            className="bg-verde-musgo text-white px-4 py-2 rounded-md hover:bg-[#154d25]"
+            className="bg-[#CCBB8E] text-black px-4 py-2 rounded-md hover:bg-[#BF8D30]"
           >
             Iniciar Sesión
           </Link>
